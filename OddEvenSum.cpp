c2f39676b6f463401eadf2754002cf02314bbd47
Aleksandar <Sasho80@gbg.bg>
2023-06-06 21:39:36 +0300

    Add files via upload

---
+++ OddEvenSum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int evenSum = 0;
+	int oddSum = 0;
+	for (int i = 0; i < n ; i++ )
+	{
+		int num;
+		cin >> num;
+		if (i % 2 == 0) {
+			evenSum += num;
+		}
+		else {
+			oddSum += num;
+		}
+	}
+	if (abs(evenSum - oddSum) == 0) {
+		cout << "Yes,sum= " << evenSum << endl;
+	}
+	else {
+		cout << "No,diff= " << abs(evenSum - oddSum) << endl;
+	}
+}
