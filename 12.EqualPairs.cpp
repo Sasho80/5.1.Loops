60fb842911659277e2d13984582db1eaacf82b05
Aleksandar <Sasho80@gbg.bg>
2023-06-18 18:29:06 +0300

    Update 12.EqualPairs.cpp

--- 12.EqualPairs.cpp
+++ 12.EqualPairs.cpp
@@ -9,6 +9,8 @@ int main() {
 	int sum = 0;
 	int diff = 0;
 	int previousSum = 0;
+	int previousDiff = 0;
+	int maxdiff = 0;
 	if (n==1)
 	{
 		n = 2 * n;
@@ -28,46 +30,41 @@ int main() {
 			}
 		}
 	}
-	else if(n==2){
+	else if(n>=2){
 		n = 2 * n;
 		for (i=1; i<=n / 2; i++)
 		{
-			sum = 0;
-			for (j = i; j <=i + 1;)
+			sum = 0; diff = 0;
+			for (j = 1; j <=2;j++)
 			{
 				int num;
 				cin >> num;
 				sum += num;
-				diff = abs(diff - num);
-				j++;
-				if (j == i + 1) {
+				diff = diff - num;
+				if (j == 2) {
+					if (i == 1) {
+						previousDiff = diff;
 						previousSum = sum;
+					}
+					else if ((i > 1) && (previousSum == sum)) {
+						previousSum = sum;
+					}
+					else if ((maxdiff) < abs(previousDiff - diff)) {
+						maxdiff = abs(previousDiff - diff);
+						previousDiff = diff;
+					}
+					else {
+						previousDiff = diff;
+					}
 				}
 			}
 		}
-	}
-	else if (n==3){
-		n = 2 * n;
-			for (i = 1; i <= n / 2; i++)
-			{
-				sum = 0;
-					for (j = i; j <= i + 1;j++)
-					{
-						int num;
-							cin >> num;
-							sum += num;
-							diff = abs(diff - num);
-						if (j == i + 1) {
-							previousSum = sum;
-						}
-					}
-			}
 	}
 	if (previousSum==sum){
 		cout << "Yes,value=" << sum << endl;
 	}
 	else{
-		cout << "No,maxdiff=" << diff<< endl;
+		cout << "No,maxdiff=" << maxdiff<< endl;
 	}
 	return 0;
 }
